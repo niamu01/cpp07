f92002db2143bfa1da27f1e22bbfb2e1264fb1e1
Yeeun Ju <yeju@c6r1s2.42seoul.kr>
2022-04-12 16:58:47 +0900

    s

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: yeju <yeju@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 12:57:01 by yeju              #+#    #+#             */
-/*   Updated: 2022/04/12 16:33:15 by yeju             ###   ########.fr       */
+/*   Updated: 2022/04/12 16:55:58 by yeju             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,7 @@
 #include <iostream>
 
 template <typename T>
-void plus(T& a)
-{
-    ++a;
-};
-
-template <typename T>
-void iter(T *address, size_t len, void (*f)(T &))
+void iter(T *address, size_t len, void (*f)(const T &))
 {
     for (size_t i = 0; i < len; i++)
     {
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,39 +6,30 @@
 /*   By: yeju <yeju@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 13:13:33 by yeju              #+#    #+#             */
-/*   Updated: 2022/04/12 16:33:15 by yeju             ###   ########.fr       */
+/*   Updated: 2022/04/12 16:56:25 by yeju             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-void str_func(std::string &str)
+void str_func(std::string const &str)
 {
     std::cout << str << std::endl;
 }
 
-void num_func(int &i)
+void num_func(int const &i)
 {
     std::cout << i << std::endl;
-}
-
-void plus_func(int &a)
-{
-    plus(a);
-    std::cout << a << std::endl;
 }
 
 int main ()
 {
     std::string str[] = {"hello", "world", "!"};
     int num[] = {1, 2, 3, 4};
-    int arr[] = {5, 6};
 
-    iter(str, 4, str_func);
+    iter(str, 3, str_func);
     std::cout << std::endl;
     iter(num, 3, num_func);
-    std::cout << std::endl;
-    iter(arr, 2, plus_func);
 
     return 0;
 }
